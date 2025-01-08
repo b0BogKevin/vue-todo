2481ffe9a23e6e808b513605c563c8b97be37d63
b0bogkev <b0bogkev@vasvari.org>
2025-01-08 11:30:19 +0100

    title

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
  
       <nav>
-        <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/tasks">Tasks</RouterLink>
         <RouterLink to="/newTask">New Task</RouterLink>
         <RouterLink to="/editTask">Edit Task</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,6 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    
+    <h1>Feladat követő rendszeralkalmazás :)</h1>
   </main>
 </template>
